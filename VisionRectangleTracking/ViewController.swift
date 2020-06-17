9b5759ee1cc42111554636accde30d954edacbee
Viennarz Curtiz <viennarzc@gmail.com>
2020-06-17 12:15:49 +0800

    Initial Commit

---
+++ VisionRectangleTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionRectangleTracking
+//
+//  Created by SCI-Viennarz on 6/17/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
